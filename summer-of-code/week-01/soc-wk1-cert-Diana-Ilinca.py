0098f80505b503ebebb782b4467eb8d941eee0b3
dianaproca <procadiana@gmail.com>
2018-07-18 18:35:03 +0100

    Update soc-wk1-cert-Diana-Ilinca.py

--- summer-of-code/week-01/soc-wk1-cert-Diana-Ilinca.py
+++ summer-of-code/week-01/soc-wk1-cert-Diana-Ilinca.py
@@ -1,11 +1,11 @@
 # soc-wk1-cert-Diana-Ilinca.py
-#hours in a year
+#hours in a year:8760
 print(365*24)
-#minutes in a decade
+#minutes in a decade: 5256000
 print(60*24*365*10)
-#age in seconds
+#age in seconds:1135296000
 print(60*60*24*365*36)
-#days 32-bit system to timeout
+#days 32-bit system to timeout:497
 print((2**32-1)/100/60/60/24)
-#days 64-bit system to timeout
+#days 64-bit system to timeout:1067519911673
 print((2**63)/100/60/60/24)
