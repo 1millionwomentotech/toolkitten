2072403bccd46c4f430c0ec92ee93d18b76857bb
dianaproca <procadiana@gmail.com>
2018-07-18 18:29:04 +0100

    Homework Day 1
    Please see my homework for day 1

---
+++ summer-of-code/week-01/soc-wk1-cert-Diana-Ilinca.py
@@ -0,0 +1,11 @@
+# soc-wk1-cert-Diana-Ilinca.py
+#hours in a year
+print(365*24)
+#minutes in a decade
+print(60*24*365*10)
+#age in seconds
+print(60*60*24*365*36)
+#days 32-bit system to timeout
+print((2**32-1)/100/60/60/24)
+#days 64-bit system to timeout
+print((2**63)/100/60/60/24)
