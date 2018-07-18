defb6aec671913816b11bbd07b1453089d6d8114
anikoBarat <aniko.barat@gmail.com>
2018-07-18 19:36:44 +0200

    Some refactoring

--- summer-of-code/week-01/calc_1.py
+++ summer-of-code/week-01/calc_1.py
@@ -33,42 +33,35 @@ def age_in_seconds(age):
 def year_from_sec(second):
     print(str(((((int(second) / 60) / 60) / 24) / 365)))
 
+
 def age_from_sec(second):
-    years = second // (60 * 60 * 24 * 365) 
-    second -= years * (60 * 60 * 24 * 365)
+    years = second // (60 * 60 * 24 * 364) 
+    second -= years * (60 * 60 * 24 * 364)
     months = second // (30 * 24 * 60 * 60)
     second -= months * (30 * 24 * 60 * 60)
     days = second // (24 * 60 * 60)
-
     print("You are " + str(years) + " years " + str(months) + " months " + str(days) + " days old.")
+
 
 # Calculate your age accurately based on your birthday
 def accurate_age(b_year, b_month, b_day):
     actual_year = 2018
     actual_month = 7
     actual_day = 18
-    actual_year_to_sec = (actual_year - 1900) * 365 * 24 * 60 * 60
+    actual_year_to_sec = (actual_year - 1900) * 364 * 24 * 60 * 60
     actual_month_to_sec = (actual_month - 1) * 30 * 24 * 60 * 60
     actual_day_to_sec = (actual_day - 1) * 24 * 60 * 60
 
-    birtday_year_to_sec = (b_year - 1900) * 365 * 24 * 60 * 60
+    birtday_year_to_sec = (b_year - 1900) * 364 * 24 * 60 * 60
     birtday_month_to_sec = (b_month - 1) * 30 * 24 * 60 * 60
     birthday_day_to_sec = (b_day - 1) * 24 * 60 * 60
 
-    actual_date_in_sec = actual_year_to_sec + actual_month_to_sec + actual_day_to_sec
-    bday_date_in_sec = birtday_year_to_sec + birtday_month_to_sec + birthday_day_to_sec
+    actual_date_after_1900_in_sec = actual_year_to_sec + actual_month_to_sec + actual_day_to_sec
+    bday_date_after_1900_in_sec = birtday_year_to_sec + birtday_month_to_sec + birthday_day_to_sec
 
-    actual_age_in_sec = actual_date_in_sec - bday_date_in_sec
+    actual_age_in_sec = actual_date_after_1900_in_sec - bday_date_after_1900_in_sec
 
     age_from_sec(actual_age_in_sec)
-
-    
-
-    
-
-
-
-
 
 print_solution(hours_per_year(), "hours", "year")
 
