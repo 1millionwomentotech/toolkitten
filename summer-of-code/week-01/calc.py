8bb86016c883d6463e1665ffcbee9084e16cb7d7
anikoBarat <aniko.barat@gmail.com>
2018-07-18 13:51:20 +0200

    Some progress at the age calculating task

--- summer-of-code/week-01/calc.py
+++ summer-of-code/week-01/calc.py
@@ -1,9 +1,12 @@
+import datetime
+
 # calculator
 # Write a program that tells you the following:
 # -Hours in a year. How many hours are in a year?
 # -Minutes in a decade. How many minutes are in a decade?
 # -Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
 # -Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+# -Calculate your age accurately based on your birthday (maybe use time of day e.g. 8:23am if you know it, use 12:00 noon midday) - you will need Python modules.
 
 
 # Print solution
@@ -31,6 +34,14 @@ def year_from_sec(second):
     print(str(((((int(second) / 60) / 60) / 24) / 365)))
 
 
+# Calculate your age accurately based on your birthday
+def accurate_age(year, month, day):
+    year = year
+    actual_year = 2018
+    year_to_sec = ((((actual_year - year) * 365) * 24) * 60) * 60
+    print(year_to_sec)
+
+
 print_solution(hours_per_year(), "hours", "year")
 
 print_solution(minutes_per_decade(), "minutes", "decade")
@@ -38,6 +49,8 @@ print_solution(minutes_per_decade(), "minutes", "decade")
 age_in_seconds(30)
 
 year_from_sec(48618000)
+
+accurate_age(1988, 7, 19)
 
 print(1+2)
 print(3)
