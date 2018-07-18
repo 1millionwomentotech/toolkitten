7360e30aa18436a3a90b88f4e4b70907cd20eaf7
Christina Hsieh <hsiehkunlun@hotmail.com>
2018-07-18 15:36:12 -0400

    feat: dfs to count tiles

--- summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-christina-hsieh.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-christina-hsieh.py
@@ -2,22 +2,53 @@ import random
 from pprint import pprint
 
 # define size of world
-size = 5 # might use user input in future
+size = 11 # might use user input in future
 
 # generate world using list comprehension
 world = [[random.randint(0,1) for x in range(size)] for x in range(size)]
+pprint(world)
 
 # get random starting point
-def get_start(size, matrix):
-  x = random.randint(0,size-1)
-  y = random.randint(0,size-1)
+def get_start(matrix):
+  x = random.randint(0, len(matrix) - 1)
+  y = random.randint(0, len(matrix) - 1)
   if matrix[x][y]:
-    return x, y
-  else:
-    get_start(size, matrix)
+    return (x, y)
+  return get_start(matrix)
 
-startX, startY = get_start(size, world)
+start = get_start(world)
 
-pprint(world)
-print(startX)
-print(startY)
+print(start)
+
+# walk from starting point
+# use stack to track path and set to track count
+path = [start]
+land = set()
+def walk(point, matrix, stack, land):
+  # add point to land
+  land.add(point)
+  # check adjacent points
+  for i in range(-1, 2):
+    for j in range(-1, 2):
+      # get coord of new point
+      x = point[0] + i
+      y = point[1] + j
+      # make sure new point is within the world
+      if x < 0 or x >= len(matrix):
+        x = point[0]
+      if y < 0 or y >= len(matrix):
+        y = point[1]
+      # check if new point is land and not counted yet
+      if matrix[x][y] and (x, y) not in land:
+        # add new point to land
+        path.append((x, y))
+        # move to new point
+        walk((x, y), matrix, stack, land)
+  # reach end of land, retrace last step
+  if stack:
+    # back = stack.pop()
+    walk(stack.pop(), matrix, stack, land)
+
+walk(start, world, path, land)
+
+print(len(land))
