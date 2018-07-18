4d9fb3c87230ba36aabda24a429771838077de45
anikoBarat <aniko.barat@gmail.com>
2018-07-18 18:59:15 +0200

    Almost accurate age from seconds  calculator function is ready

--- summer-of-code/week-01/calc_1.py
+++ summer-of-code/week-01/calc_1.py
@@ -33,13 +33,41 @@ def age_in_seconds(age):
 def year_from_sec(second):
     print(str(((((int(second) / 60) / 60) / 24) / 365)))
 
+def age_from_sec(second):
+    years = second // (60 * 60 * 24 * 365) 
+    second -= years * (60 * 60 * 24 * 365)
+    months = second // (30 * 24 * 60 * 60)
+    second -= months * (30 * 24 * 60 * 60)
+    days = second // (24 * 60 * 60)
+
+    print("You are " + str(years) + " years " + str(months) + " months " + str(days) + " days old.")
 
 # Calculate your age accurately based on your birthday
-def accurate_age(year, month, day):
-    year = year
+def accurate_age(b_year, b_month, b_day):
     actual_year = 2018
-    year_to_sec = ((((actual_year - year) * 365) * 24) * 60) * 60
-    print(year_to_sec)
+    actual_month = 7
+    actual_day = 18
+    actual_year_to_sec = (actual_year - 1900) * 365 * 24 * 60 * 60
+    actual_month_to_sec = (actual_month - 1) * 30 * 24 * 60 * 60
+    actual_day_to_sec = (actual_day - 1) * 24 * 60 * 60
+
+    birtday_year_to_sec = (b_year - 1900) * 365 * 24 * 60 * 60
+    birtday_month_to_sec = (b_month - 1) * 30 * 24 * 60 * 60
+    birthday_day_to_sec = (b_day - 1) * 24 * 60 * 60
+
+    actual_date_in_sec = actual_year_to_sec + actual_month_to_sec + actual_day_to_sec
+    bday_date_in_sec = birtday_year_to_sec + birtday_month_to_sec + birthday_day_to_sec
+
+    actual_age_in_sec = actual_date_in_sec - bday_date_in_sec
+
+    age_from_sec(actual_age_in_sec)
+
+    
+
+    
+
+
+
 
 
 print_solution(hours_per_year(), "hours", "year")
