680f520bca58e509d6e596c1f885f5f74b756593
Christina Hsieh <hsiehkunlun@hotmail.com>
2018-07-17 22:50:56 -0400

    feat: random start point

--- summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-christina-hsieh.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-christina-hsieh.py
@@ -1,7 +1,23 @@
 import random
+from pprint import pprint
 
 # define size of world
 size = 5 # might use user input in future
 
 # generate world using list comprehension
 world = [[random.randint(0,1) for x in range(size)] for x in range(size)]
+
+# get random starting point
+def get_start(size, matrix):
+  x = random.randint(0,size-1)
+  y = random.randint(0,size-1)
+  if matrix[x][y]:
+    return x, y
+  else:
+    get_start(size, matrix)
+
+startX, startY = get_start(size, world)
+
+pprint(world)
+print(startX)
+print(startY)
