5973316e9b1dbf3401150a4f96cc45a0a405b4ba
anikoBarat <aniko.barat@gmail.com>
2018-07-18 14:11:10 +0200

    Change some file name to avoid conflicting files at syncing the forked repo with the remote

---
+++ summer-of-code/week-01/calc_2.py
@@ -0,0 +1,49 @@
+
+# print(1+2)
+# print(3)
+
+# print(10%2)
+# print(11%2)
+
+# for i in range(0,9):
+#   print("bitshift ", i, "times ", 1<<i)
+
+# print('Hello, world!')
+# print('')
+# print('Good-bye.')
+
+# print( 'I like' + 'chocolate cake.' )
+# print('I like ' + 'apple pie.')
+# print('I like' + ' apple pie.')
+
+# print('blink ' * 4)
+# print('moo'*3)
+# print('moo'/3)
+
+# print(12 + 12)
+# print('12' + '12')
+# print('12 + 12')
+
+# print(2 * 5)
+# print('2' * 5)
+# print('2 * 5')
+
+# print('12' + 12)
+#print('12' + str(12))
+
+# print('2' * '5')
+
+# print('Betty' + 12)
+# print('Francesca' * 'Hanna')
+
+print('You\'re swell!')
+print('backslash at the end of a string: \\')
+print('up\\down')
+print('up\down')
+
+
+
+
+
+
+
