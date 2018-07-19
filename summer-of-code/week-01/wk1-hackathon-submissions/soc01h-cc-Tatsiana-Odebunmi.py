b9023eb1885d4e91c5dafcfad404207dbeacde29
Tatsiana <segl.tanya@gmail.com>
2018-07-19 14:31:02 -0400

    reduced amount of global variables

--- summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-Tatsiana-Odebunmi.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-Tatsiana-Odebunmi.py
@@ -2,17 +2,17 @@
 import random
 import numpy as np
 import math
-index=11
-x_start=0
-y_start=0
-num_lands=[] #array of sizes of land
-x_list=[] #array of checked points(x)
-y_list=[] #array of checked points(y)
+class Error(Exception):
+	"""docstring for Error"""
+	pass
+class WaterValueError(Error):
+	"""docstring for ClassName"""
+	pass
+
 x_cur_list=[] #array of curent continet points(x)
 y_cur_list=[] #array of curent continet points(y)
-sq=pow(index,2)
-world=np.arange(sq).reshape(index,index) #index*index world
-
+x_list=[] #array of checked points(x)
+y_list=[] #array of checked points(y)
 #checking if curent point of land is in the list of lands, if not will add it to the list
 def check_availability(x,y):
 	if len(x_cur_list)>0:
@@ -46,36 +46,42 @@ def checked_coordinates(x,y):
 		return 0
 #def find_next_continent
 #recursion to check each point of the grid
-def if_those_land_conected(x,y,index):
+def if_those_land_conected(x,y,index,world):
 	check=int(checked_coordinates(x,y))
 	if check==0:
 		if world[x][y]==1:
 			check_availability(x,y)
 			if (y+1)<index:
-				if_those_land_conected(x,(y+1),index)
+				if_those_land_conected(x,(y+1),index,world)
 				if (x+1)<index:
-					if_those_land_conected((x+1),(y+1),index)
+					if_those_land_conected((x+1),(y+1),index,world)
 				if(x-1)>=0:
-					if_those_land_conected((x-1),(y+1),index)
+					if_those_land_conected((x-1),(y+1),index,world)
 			if (x+1)<index:
-				if_those_land_conected((x+1),y,index)
+				if_those_land_conected((x+1),y,index,world)
 				if (y-1)>=0:
-					if_those_land_conected((x+1),(y-1),index)
+					if_those_land_conected((x+1),(y-1),index,world)
 			if (x-1)>=0:
-				if_those_land_conected((x-1), y,index)
+				if_those_land_conected((x-1), y,index,world)
 				if (y-1)>=0:
-					if_those_land_conected((x-1),(y-1),index)
+					if_those_land_conected((x-1),(y-1),index,world)
 			if(y-1)>=0:
-				if_those_land_conected(x,(y-1),index)
+				if_those_land_conected(x,(y-1),index,world)
 		else:
 			if (len(x_cur_list)==0):
 				if (y+1)<index:
-					if_those_land_conected(x,(y+1),index)
+					if_those_land_conected(x,(y+1),index,world)
 				else:
 					if (x+1)<index:
-						if_those_land_conected((x+1), 0,index)
+						if_those_land_conected((x+1), 0,index,world)
 # Main function
 def main():
+	x_start=0
+	y_start=0
+	index=11
+	num_lands=[] #array of sizes of land
+	sq=pow(index,2)
+	world=np.arange(sq).reshape(index,index) #index*index world
 	while True:
 		try:
 			index=int(input("Input the size of world x*x (1...100): "))
@@ -86,7 +92,6 @@ def main():
 			break
 		except ValueError:
 			print("No valid symbol! Please try again...")
-
 	#randomly filling the world with numbers 0 & 1
 	for x in range(index):
 		for y in range(index):
@@ -97,31 +102,30 @@ def main():
 		try:
 			x_start=int(input("Input your start x coordinate (0...%s): " %(index-1)))
 			y_start=int(input("Input your start y coordinate (0...%s): " %(index-1)))
-			if x>=index or x<0:
+			if x_start>=index or x_start<0:
 				raise ValueError
-			if y>=index or y<0:
+			if y_start>=index or y_start<0:
 				raise ValueError
 			if world[x_start][y_start]==0:
-				raise ValueError('Ooops it is water!!! Please try again...')
+				raise WaterValueError()
 			if world[x_start][y_start]==1:
 				print("Yahoo it is a land!!!")
 			break
-		except ValueError as err:
-			print(err.args)
+		except WaterValueError as Er:
+			print("Ooops it is water!!! Please try again... \n")
 		except ValueError:
 			print("No valid symbol! Please try again...")
 	for x in range(index):
 		for y in range(index): #printing creted word in format x*x
 			print (world[x,y], end='')
 		print("")
-	"""
-	world=np.arange(25).reshape(5,5) #5x5 world #1
+	"""	world=np.arange(25).reshape(5,5) #5x5 world #1
 	world=[[0,1,0,0,1],[1,0,0,1,0],[1,0,0,0,0],[1,0,1,1,1],[0,0,1,1,1]]
 	index=5"""
-	if_those_land_conected(x_start,y_start,index)
+	if_those_land_conected(x_start,y_start,index,world)
 	for x in range(index):
 		for y in range(index):
-			if_those_land_conected(x,y,index)
+			if_those_land_conected(x,y,index,world)
 			if len(x_cur_list)>0:
 				num_lands.append(len(x_cur_list))
 			x_cur_list.clear()
