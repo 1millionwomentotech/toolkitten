5f3f0bda370082733497f657d485ce4d32c27367
bexpe <beata.k.puzio@gmail.com>
2018-07-20 01:02:06 +0200

    Add extension of the problem to the n x n size world

--- summer-of-code/week-01/wk1-hackathon-submissions/continent_counter.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/continent_counter.py
@@ -3,11 +3,13 @@ import operator
 
 list_of_tiles = []
 list_of_close_tiles = []
+world_size_x = int(input('Welcome! Now enter an integer. Your world should have size of: '))
+world_size_y = int(input('Since we want it to be a 2D world, please enter another integer of your choice: '))
 
 
 def generate_random_world():
     random.seed()
-    list_of_tiles = [(random.randint(0, 11), random.randint(0, 11)) for k in range(30)]  # here we have to add some code to not repeat tiles
+    list_of_tiles = [(random.randint(0, world_size_x), random.randint(0, world_size_y)) for k in range(30)]  # here we have to add some code to not repeat tiles
     return list_of_tiles
 
 
@@ -22,7 +24,6 @@ def add_default_coordinates(coordinates):
 
 
 def count_area(list_of_tiles, coordinates, list_of_close_tiles):
-    print(list_of_close_tiles)
     for item in list_of_tiles:
         if item[0] == coordinates[0] or item[0] == coordinates[0] - 1 or item[0] == coordinates[0] + 1:
             if item[1] == coordinates[1] or item[1] == coordinates[1] - 1 or item[1] == coordinates[1] + 1:
