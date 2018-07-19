f7f6084f706ac3e180d6d359ccd6ceb122bbb017
bexpe <beata.k.puzio@gmail.com>
2018-07-20 01:02:45 +0200

    Add some print

--- summer-of-code/week-01/wk1-hackathon-submissions/main.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/main.py
@@ -7,7 +7,7 @@ def main():
     list_of_tiles = continent_counter.generate_random_world()
     sorted_list_of_tiles = continent_counter.sort_list_of_tiles_by_x_y(list_of_tiles)
     list_of_close_tiles = continent_counter.count_area(sorted_list_of_tiles, coordinates, list_of_close_tiles)
-    print(list_of_close_tiles)
+    print("These are coordinates of your continent: " + str(list_of_close_tiles))
     print("The area of your continent is: " + str(len(list_of_close_tiles)))
 
 
