1c696ab6322bdd9500ea176140918a96d2d90b7e
Paula Bernal <paulabernal@Paulas-MacBook-Air.local>
2018-07-19 21:53:07 +0200

    added random world generator

--- summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-paula-bernal.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/soc01h-cc-paula-bernal.py
@@ -26,4 +26,19 @@ print(size)
 
 
 #returns 32 for this example
+
+
+#Create a random world 11x11 tiles
+
+import random
+options = ['w','L']
+world = list()
+for x in range (11):
+	col = list()
+	for y in range (11):
+		tile = random.choice(options)
+		col.append(tile)
+	world.append(col)
+	
+
 	
