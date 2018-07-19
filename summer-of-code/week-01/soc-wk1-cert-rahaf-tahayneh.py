5a3790319602a5597f05ed42af3737ca03f99c4e
Rahaf Tahayneh <rahaf.tahaineh@gmail.com>
2018-07-19 11:40:28 +0300

    Create soc-wk1-cert-rahaf-tahayneh.py

---
+++ summer-of-code/week-01/soc-wk1-cert-rahaf-tahayneh.py
@@ -0,0 +1,21 @@
+# Hours in a year
+print(365*24)
+
+# Minutes in a decade
+print(10*365*24*60)
+
+# My age in seconds
+print(21*365*24*60*60)
+
+# @Andreea Visanoiu's age.
+print( 48618000 / (365*24*60*60))
+
+# days 32-bit system to timeout
+print((2**31) /1000/24/60/60)
+
+
+# days 64-bit system to timeout
+print((2**63) /1000/24/60/60)
+
+
+
