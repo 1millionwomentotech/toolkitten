9cfaaa96e02522d598720a1a3b30760a47de962b
Rahaf Tahayneh <rahaf.tahaineh@gmail.com>
2018-07-19 23:09:45 +0300

    Create soc01-cc-rahaf-tahayneh.py

---
+++ summer-of-code/week-01/wk1-homework-submission/soc01-cc-rahaf-tahayneh.py
@@ -0,0 +1,55 @@
+
+#######Day1 Homework
+
+# Hours in a year
+print(365*24)
+
+# Minutes in a decade
+print(10*365*24*60)
+
+# My age in seconds
+print(21*365*24*60*60)
+
+# @Andreea Visanoiu's age.
+print( 48618000 / (365*24*60*60))
+
+# days 32-bit system to timeout
+print((2**31) /1000/24/60/60)
+
+
+# days 64-bit system to timeout
+print((2**63) /1000/24/60/60)
+
+########################################################
+#######Day3 Homework
+print('Enter Your first name')
+n1 =input()
+print('Enter Your Middle name')
+n2 = input()
+print('Enter Your last name')
+n3 = input()
+print(n1+ ' ' + n2 + ' '+ n3)
+
+###############################
+print('Enter your favorite number')
+num = input()
+result= num+1
+print(result + ' ' )
+print('Bigger, better favorite number')
+
+###############################
+request=input('please input your request here: ')
+print("WHADDAYA MEAN \""+request+"\"?!? YOU\'RE FIRED!!")
+
+###############################
+title='Table of Contents'
+chapter='Chapter 1: Getting Started page 1 Chapter 2: Numbers page 9 Chapter 3: Letters page 13'
+
+print(title.ljust(80))
+print(chapter.center(85))
+
+
+###############################
+
+
+
