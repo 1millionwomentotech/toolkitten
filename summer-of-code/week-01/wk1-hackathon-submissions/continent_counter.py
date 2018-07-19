9434dffa109621437f336ea528349f309469b11a
bexpe <beata.k.puzio@gmail.com>
2018-07-20 00:29:16 +0200

    Change for fuctional programming

--- summer-of-code/week-01/wk1-hackathon-submissions/continent_counter.py
+++ summer-of-code/week-01/wk1-hackathon-submissions/continent_counter.py
@@ -3,18 +3,29 @@ import operator
 
 list_of_tiles = []
 list_of_close_tiles = []
-coordinates = (5, 5)
 
-random.seed()
-list_of_tiles = [(random.randint(0, 11), random.randint(0, 11)) for k in range(30)]  # here we have to add some code to not repeat tiles
 
-sorted_by_x = sorted(list_of_tiles, key=operator.itemgetter(0, 1))
-list_of_close_tiles.append(coordinates)
+def generate_random_world():
+    random.seed()
+    list_of_tiles = [(random.randint(0, 11), random.randint(0, 11)) for k in range(30)]  # here we have to add some code to not repeat tiles
+    return list_of_tiles
 
-for item in list_of_tiles:
-    if item[0] == coordinates[0] or item[0] == coordinates[0] - 1 or item[0] == coordinates[0] + 1:
-        if item[1] == coordinates[1] or item[1] == coordinates[1] - 1 or item[1] == coordinates[1] + 1:
-            if item != coordinates:
-                list_of_close_tiles.append(item)
 
-print(list_of_close_tiles)
+def sort_list_of_tiles_by_x_y(list_of_tiles):
+    list_of_tiles = sorted(list_of_tiles, key=operator.itemgetter(0, 1))
+    return list_of_tiles
+
+
+def add_default_coordinates(coordinates):
+    list_of_close_tiles.append(coordinates)
+    return list_of_close_tiles
+
+
+def count_area(list_of_tiles, coordinates, list_of_close_tiles):
+    print(list_of_close_tiles)
+    for item in list_of_tiles:
+        if item[0] == coordinates[0] or item[0] == coordinates[0] - 1 or item[0] == coordinates[0] + 1:
+            if item[1] == coordinates[1] or item[1] == coordinates[1] - 1 or item[1] == coordinates[1] + 1:
+                if item != coordinates:
+                    list_of_close_tiles.append(item)
+    return list_of_close_tiles
