7f9ad71282689d05a108bf21a1beb3e4bd8e19fd
Smruti Shah <>
2018-07-19 20:07:13 -0500

    initial commit

---
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-Smruti_Shah.py
@@ -0,0 +1,28 @@
+# Week 1, Day 1
+# Hours in a year. How many hours are in a year?
+
+totalHours = 365 * 24
+print("There are " + str(totalHours) + " hours in a year")
+
+# Minutes in a decade. How many minutes are in a decade?
+
+minDec = 10 * (365 + 2) * 24 * 60
+print("There are " + str(minDec) + " minutes in a decade")
+
+# Your age in seconds. How many seconds old are you? (Be as accurate—or not—as you want.)
+# 8.39pm - 20, July 3.15am (IST)
+
+ageInSec = (19 * (365 + 5) * 24 * 60 * 60) + (6 * 24 * 60 * 60) + (200 * 24 * 60 * 60) + (6 * 60 * 60) + (36 * 60)
+print("I'm " + str(ageInSec) + " seconds old")
+
+# Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+
+andreaAge = 48618000 / 60 / 60 / 24 / 365
+print("Andreea Visanoiu's age is " + str(andreaAge))
+
+
+# Tougher Questions
+# How many days does it take for a 32-bit and 64-bit system to timeout, if it has a bug with integer overflow?
+# Can someone please send me this answer,
+# I really want to know the answer as this question has increased my curiosity
+
