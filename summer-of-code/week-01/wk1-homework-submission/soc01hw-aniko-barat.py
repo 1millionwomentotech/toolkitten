aeec1317c8160b7a99eb0d670230fefc1a90ebf7
anikoBarat <aniko.barat@gmail.com>
2018-07-20 21:16:17 +0200

    homework @ some hackathon progress

--- summer-of-code/week-01/wk1-homework-submission/soc01hw-aniko-barat.py
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-aniko-barat.py
@@ -135,7 +135,9 @@ https://docs.python.org/3/library/stdtypes.html#str
 '''
 
 '''5)
-- Angry boss. Write an angry boss program that rudely asks what you want. Whatever you answer, the angry boss should yell it back to you and then fire you. For example, if you type in I want a raise, it should yell back like this: 
+- Angry boss. Write an angry boss program that rudely asks what you want. 
+Whatever you answer, the angry boss should yell it back to you and then fire you. 
+For example, if you type in I want a raise, it should yell back like this: 
 
 ```
 WHADDAYA MEAN "I WANT A RAISE"?!? YOU'RE FIRED!!
