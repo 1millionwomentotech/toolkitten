5f1cb03decb839052782e0652fd22f421d35734b
Chelsea Evans <41181159+ChelseaSianEvans@users.noreply.github.com>
2018-07-20 08:31:03 +0200

    Add files via upload

--- soc-wk1-cert-Chelsea-Evans.py
+++ soc-wk1-cert-Chelsea-Evans.py
@@ -1,3 +1,20 @@
+# Day 1
 print (24*365)
 print (((60*24)*365)*10)
 print((((60*60)*24)*365)*26)
+
+# Day 3
+firstname = input ("What is your first name?")
+middlename = input ("What is your middle name?")
+lastname = input ("What is your last name?")
+
+print ("Welcome "firstname + middlename + lastname)
+
+# Exercise 2
+
+print("What is your favourite number:")
+x = input()
+b = int(x)
+c = (b+1)
+c = str(c)
+print ("This is better, " + c)
