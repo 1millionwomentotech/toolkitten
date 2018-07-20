048c60faa2e2fae366a6871c2fff7278a0abe922
Chelsea Evans <41181159+ChelseaSianEvans@users.noreply.github.com>
2018-07-20 10:00:56 +0200

    Add files via upload

--- soc-wk1-cert-Chelsea-Evans.py
+++ soc-wk1-cert-Chelsea-Evans.py
@@ -18,3 +18,80 @@ b = int(x)
 c = (b+1)
 c = str(c)
 print ("This is better, " + c)
+
+# Exercise 3
+
+print ("What do you want?")
+x = input()
+print("Shut Up")
+y = input()
+print ("Really")
+print("WHADDAYA MEAN I WANT A RAISE?!? YOU'RE FIRED!!")
+
+#Exercise 4
+
+print ("Table of Contents" str.center(10))
+
+print ("Chapter 1:" str.ljust(10))
+print ("Getting Started" str.center(10))
+print ("page 1" str.rjust(10))
+print ("Chapter 2:" str.ljust(10))
+print ("Numbers" str.center(10))
+print ("page 9" str.rjust(10))
+print ("Chapter 3:" str.ljust(10))
+print ("Letters" str.center(10))
+print ("page 13" str.rjust(10))
+
+#Exercise 5
+
+print "99 Beers on the wall"
+
+# Exercise 6
+
+input= ""
+print ("HUH?! SPEAK UP, SONNY!")
+
+input = ""
+print ("NO, NOT SINCE 1938")
+
+input = ""
+print ("NO, NOT SINCE" + random.randint(1930,1950) + "!")
+
+input = ""
+print ("NO, NOT SINCE" + random.randint(1930,1950) + "!")
+
+input = ""
+print ("NO, NOT SINCE" + random.randint(1930,1950) + "!")
+
+input = ""
+print ("NO, NOT SINCE" + random.randint(1930,1950) + "!")
+
+input = ""
+print ("NO, NOT SINCE" + random.randint(1930,1950) + "!")
+
+input = ""
+print ("NO, NOT SINCE" + random.randint(1930,1950) + "!")
+
+input = ""
+print ("Hello dear")
+
+input = ""
+print ("Hello dear")
+
+input = ""
+print ("Hello dear")
+
+input = ""
+​while​ input != ​'bye'​:
+  input = input()
+​
+print(​'Come again soon!')​
+
+#Exercise 7
+
+words = input ("Write as many words as you want:")
+sorted(words)
+print ("The sorted words are:")
+for word in words:
+    print(word)
+
