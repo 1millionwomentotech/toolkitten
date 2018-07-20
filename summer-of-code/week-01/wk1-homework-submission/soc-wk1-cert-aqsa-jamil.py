3a0c2cc34e700fad8578eb3bc5f0b85435bf1f36
aqsa <aqsajamil476@gmail.com>
2018-07-20 01:11:19 -0700

    first commit

---
+++ summer-of-code/week-01/wk1-homework-submission/soc-wk1-cert-aqsa-jamil.py
@@ -0,0 +1,94 @@
+#Hours in a year. How many hours are in a year?
+
+print(365*24)
+
+#Minutes in a decade. How many minutes are in a decade?
+print( 10*365*24*60)
+
+#Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
+print(21*365*24*60*60)
+
+#Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+print(365*24*60*60)
+print(662252000/31536000)
+
+#How many days does it take for a 32-bit system to timeout, if it has a bug with integer
+#overflow?
+print(2**31)
+print(2147483647/60/60/24)
+
+print(24855//365.25+1970)
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+print(datetime.now() + timedelta(days = 24855))
+
+#Calculate your age accurately based on your birthday (maybe use time of day e.g. 8:23am if you know it, use 12:00 noon midday) - you will need Python modules.
+print(2**21)
+print(2097152/60/60/24)
+print(24.27259259259259//365.25+1998)
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+print(datetime.now() + timedelta(days = 24.27259259259259))
+
+# variable assigning values
+var1 = 2
+var2 = '5'
+print (str(var1) + (var2))
+print(var1 + int(var2))
+print(var1)
+print(var2)
+#input name from user
+first_name=input("enter your first name:")
+middle_name=input("enter your middle name:")
+last_name=input("enter your last name:")
+print(first_name +middle_name + last_name)
+ #day 2
+
+ 
+print(1+2)
+print(3)
+print(1+2)
+print(3)
+
+print(10%2)
+print(11%2)
+for i in range(0,9):
+                  print("bitshift ", i, "times ", 1<<i)
+
+print('Hello, world!')
+print('')
+print('Good-bye.')
+
+print( 'I like' + 'chocolate cake.' )
+print('I like ' + 'apple pie.')
+print('I like' + ' apple pie.')
+#//////////////
+
+print(12 + 12)
+print('12' + '12')
+print('12 + 12')
+#/////////
+print(2 * 5)
+print('2' * 5)
+print('2 * 5')
+
+
+print('12' + str(12))
+
+
+print('Betty' + 12)
+print('Francesca' * 'Hanna')
+
+print('You\'re swell!')
+print('backslash at the end of a string: \\')
+print('up\\down')
+print('up\down')
+
+ 
+print(10%2)
+print(11%2) 
+          
