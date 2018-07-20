d3869807e155f9faf67ef8a31614d49654b8b4e3
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-07-20 19:34:35 +0100

    added an exercise I had missed

--- summer-of-code/week-01/wk1-homework-submission/soc-wk1-cert-cristina-tarantino.py
+++ summer-of-code/week-01/wk1-homework-submission/soc-wk1-cert-cristina-tarantino.py
@@ -156,7 +156,7 @@ print("No more bottles of beer on the wall, no more bottles of beer. "
 # NO, NOT SINCE 1938!
 # To make your program really believable, have Grandma shout a different year each time,
 # maybe any year at random between 1930 and 1950.
-# You can’t stop talking to Grandma until you shout BYE.
+# You can't stop talking to Grandma until you shout BYE.
 print()
 tell_grandma = ""
 
@@ -205,12 +205,15 @@ while tell_grandma != "BYE":
 # Use string formatting such as left align, right align, center.
 
 
-# 19. Old-school Roman numerals. In the early days of Roman numerals,
+# 19. Write a function that prints out "moo" n times.
+
+
+# 20. Old-school Roman numerals. In the early days of Roman numerals,
 # the Romans didn't bother with any of this new-fangled subtraction “IX” nonsense.
 # No Mylady, it was straight addition, biggest to littlest—so 9 was written “VIIII,” and so on.
 
 
-# 20. Write a method that when passed an integer between 1 and 3000 (or so)
+# 21. Write a method that when passed an integer between 1 and 3000 (or so)
 # returns a string containing the proper old-school Roman numeral.
 # In other words, old_roman_numeral 4 should return 'IIII'.
 # Make sure to test your method on a bunch of different numbers.
@@ -225,7 +228,7 @@ while tell_grandma != "BYE":
 # M = 1000
 
 
-# 21. “Modern” Roman numerals.
+# 22. “Modern” Roman numerals.
 # Eventually, someone thought it would be terribly clever if putting a smaller number before a larger one meant you
 # had to subtract the smaller one. As a result of this development, you must now suffer.
 # Rewrite your previous method to return the new-style Roman numerals so when someone calls roman_numeral 4,
