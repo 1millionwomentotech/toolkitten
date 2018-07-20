53c61dae4bd6083a23b1a6a24b6e71a6bf56c9d1
Shubhi Rustagi <shubhi.rustagi99@gmail.com>
2018-07-20 17:43:41 +0545

    Homework submission

---
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-firstname-lastname.py
@@ -0,0 +1,54 @@
+#hours in a year
+var = 24 * 365
+print("Hours in an year: " + str(var))
+
+#minutes in a decade
+var1 = var * 10 * 60
+print("Minutes in a decade: " + str(var1))
+
+#age in seconds
+var2 = var * (2018 - 1999) * 60 * 60
+print("Age in seconds: " + str(var2))
+
+#full name
+print("First Name: ")
+var3 = input()
+
+print("Middle Name: ")
+var4 = input()
+
+print("Last Name: ")
+var5 = input()
+
+print("Welcome " + var3 + " " + var4 + " " + var5)
+
+#favourite number
+print("Type in your Favourite Number: ")
+var6 = input()
+var6 = int(var6) + 1
+print("Better Favourite Number for you is: " + str(var6))
+
+#uppercase 
+print("Enter the new string: ")
+var7 = input()
+print(var7.upper())
+
+#grandma listens loud
+print("Type something so that Grandma listens.")
+var8 = input()
+if var8.islower() : 
+	print("HUH?! SPEAK UP, GIRL!")
+else : 
+    print("NO, NOT SINCE 1938!")     
+
+#sorting the lists...
+var9 = list()
+num = input("Enter the number of elements you want to enter: ")
+print("Enter the letters: ")
+for i in range(int(num)):
+	n = input("Number: ")
+	var9.append(str(n))
+print('ARRAY: ', var9)
+
+var9.sort()
+print('SORTED ARRAY: ', var9)
