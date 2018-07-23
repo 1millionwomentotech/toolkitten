6e9926e1e30544cb297a457cccbbe2f02282ddd4
thatonechick1987 <caffiendkitten@gmail.com>
2018-07-23 16:17:08 -0700

    Add files via upload

---
+++ soc-wk1-cert-DaNeil-Coulthard.py
@@ -0,0 +1,33 @@
+print("##Week1 day1 general calculations")
+print ("Some calculations for day of birth...")
+print ("The number of hours in a year: ", 365*24)
+print("The number of minutes in a decade: ", 10*365*24*60)
+print ("My age (30) in seconds: ", (30*365*24*60*60)/10)
+print("Andreea's age is", ((((48618000/365)/24)/60/60)*10))
+
+#I'm not entierly sure how the calculations for age worked but
+#I was able to get it based off mine and only when i devided it or multiplied it by 10... seems odd but it worked.
+print("")
+print("")
+print("")
+
+
+print("##Week1 day 2. Strings")
+print ("hello world")
+print("")
+print("i like " + 'apple pie')
+print ('blink ' *4)
+print ('12' + "12" )
+print ('25 ' + str( 34))
+print ('you\'re swell')
+# ^^ this works because the back slash is an excape char used
+#to ignore the single char after it.
+print("")
+
+myString = "kuu kuu kachu"
+print (myString)
+print (myString)
+print("")
+name = "DaNeil"
+print ('My name is ' +name)
+
