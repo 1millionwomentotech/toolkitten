e270261893325f32c527fd897a8a7fffdf4b025f
Deepa Tamang <tamangbkt@gmail.com>
2018-07-26 22:04:19 +0545

    Week-2 Day-2 Unittesting

---
+++ summer-of-code/week-02/test_day2.py
@@ -0,0 +1,23 @@
+import unittest
+from day2 import continent_counter
+M = 'land'
+o = 'water'
+world = [
+         [o,o,o,o,o,o,o,o,o,o,o],
+         [o,o,o,o,M,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,M,M,o],
+         [o,o,o,M,o,o,o,o,o,M,o],
+         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,o,M,M,M,M,o,o,o],
+         [o,o,o,M,M,M,M,M,M,M,o],
+         [o,o,o,M,M,o,M,M,M,o,o],
+         [o,o,o,o,o,o,M,M,o,o,o],
+         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,o,o,o]
+        ]
+class TestDay2(unittest.TestCase):
+	def test_first(self):
+		self.assertEqual(continent_counter(world,1,4), 2)
+	def test_first(self):
+		self.assertEqual(continent_counter(world,5,5),23)
+unittest.main()
