e5fa42f7ad8da78c8251c52493129ad6e5a7d1b1
Ilona Budapesti <ilona.budapesti@gmail.com>
2018-07-26 09:55:06 +0100

    add Week 2 Day 4

---
+++ summer-of-code/week-02/cool_int.py
@@ -0,0 +1,15 @@
+# cool_int.py
+
+def to_eng(n):
+    if n == 5:
+        english = 'five'
+    else:
+        english = 'forty-two'
+    print(english)
+    return english
+
+awesome_student = {
+    "name": "Elle J",
+    "city": "London",
+    "country": "UK"
+}
