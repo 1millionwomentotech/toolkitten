f14ca5ca887410e2a648d0ab729cc2e91c004490
Priyanka Saggu <priyankasaggu11929@gmail.com>
2018-07-29 03:57:34 +0530

    Delete 99-bottles.py

--- summer-of-code/week-01/99-bottles.py
+++
@@ -1,9 +0,0 @@
-#!/ usr/bin/env python3
-... for i in range(99,-1,-1):
-...     if i==0:
-...             print('No more bottles of beer on the wall, no more bottles of beer.\n Go to the store and buy some more.')
-...     elif i==1:
-...             print('1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, 0 bottles of beer on the wall\n')
-...     else:
-...             print('{no} bottles of beer on the wall, {no} bottles of beer.\nTake one down and pass it around, {non} bottles of beer on the wall.\n'.format(no=i, non=i-1))
-
