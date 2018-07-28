6b4145ef7d916d48bfa580d42dc1e3bc38ab08da
PriyankaSaggu <priyankasaggu11929@gmail.com>
2018-07-29 03:47:20 +0530

    added two programs, deaf-grandma.py and 99-bottles.py

---
+++ summer-of-code/week-01/wk1-homework-submissions/deaf-grandma.py
@@ -0,0 +1,8 @@
+while (True):
+...     x=input()
+...     y=x.upper()
+...     if x==y:
+...             print('yeah, dear!!')
+...             break
+...     else:
+...             print('HUH?! SPEAK UP, GIRL!')
