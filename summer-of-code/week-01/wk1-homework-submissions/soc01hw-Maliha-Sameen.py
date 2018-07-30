994949bcc6a8046c1773d2680812f7dc25ee78f6
Maliha Sameen <malihasameen58@gmail.com>
2018-07-30 18:31:32 +0500

    add week 1 day 3

--- summer-of-code/week-01/wk1-homework-submissions/soc01hw-Maliha-Sameen.py
+++ summer-of-code/week-01/wk1-homework-submissions/soc01hw-Maliha-Sameen.py
@@ -29,3 +29,61 @@ month = str(dt.date.today().month - dob.month)
 day = str(dt.date.today().day - dob.day)
 
 print("I'm",year,"years",month, "months", day, "days old.", sep=" ")
+
+### Week 1 Day 3 Homework
+
+## Question 1: Write a program that asks for a person’s first name, 
+## then middle, and then last. Finally, it should greet the person 
+## using their full name
+first_name = input("FirstName: ")
+middle_name = input("MiddleName: ")
+last_name = input("LastName: ")
+print("Hi", first_name, middle_name, last_name, sep=" ")
+
+## Question 2: Write a program that asks for a person’s favorite number. 
+## Have your program add 1 to the number, and then suggest the result 
+## as a bigger and better favorite number.
+try:
+	fav_number = int(input("Favourite Number: "))
+	print("A better and bigger favourite number can be ",fav_number+1)
+except:
+	print("Not A Number")
+
+## Question 3: Write an angry boss program that rudely asks what you 
+## want. Whatever you answer, the angry boss should yell it back to you 
+## and then fire you.
+answer = input("What do you want? ")
+print("WHADDAYA MEAN \"" + answer.upper() + "\"?!? YOU'RE FIRED!!")
+
+## Question 4: write a program that will display a table of contents.
+print("Table of Contents".center(50,'_'),end="\n\n")
+chapter = []
+page = []
+chapter += ["Chapter 1: Getting Started".ljust(25,'.')]
+chapter += ['Chapter 2: Numbers'.ljust(25,'.')] 
+chapter += ['Chapter 3: Letters'.ljust(25,'.')]
+page += ['page 1'.rjust(25,'.')] 
+page += ['page 9'.rjust(25,'.')] 
+page += ['page 13'.rjust(25,'.')] 
+
+for i in range(3):
+	print(chapter[i] + page[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
