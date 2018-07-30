dd9895154bcaf3261fd9006a6636660aefeb5bb4
Sameeksha <rays2210@gmail.com>
2018-07-30 20:00:54 +0530

    Day1 without tough questions

---
+++ summer-of-code/week-01/wk1-homework-submissions/soc-wk1-cert-sameeksha-mahajan.py
@@ -0,0 +1,22 @@
+####################################################### A few things to try###################################################
+
+#####DAY 1#####
+
+## Hours in a year. How many hours are in a year?
+print("Hours in a year", 24*365)
+# Hours in a year 8760
+
+## Minutes in a decade. How many minutes are in a decade?
+print("Minutes in a decade", 10*365*24*60)
+# Minutes in a decade 5256000
+
+## Your age in seconds. How many seconds old are you?
+print("My age in seconds", 23*365*24*60*60)
+# My age in seconds 725328000
+
+## Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+print("Her age:", 48618000/(60*60*24*365))
+# Her age: 1.54166
+#Strange :o
+
+## 
