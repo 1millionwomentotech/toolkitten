2282d9f5e328642921a6d89b30892800b806776d
nia-nwabuko <42580097+nia-nwabuko@users.noreply.github.com>
2018-08-22 13:43:12 +0100

    Submission for week 01 homework 01

---
+++ summer-of-code/week-01/wk1-homework-submissions/soc-wk1-cert-sonia-nwabuko.py
@@ -0,0 +1,9 @@
+# Basic exercises
+
+hrs_in_year = 365*24
+sec_in_year = hrs_in_year*3600
+
+print("There are",(hrs_in_year),"hours in a year.")
+print("There are",(10*hrs_in_year*60),"minutes in a decade.")
+print("I am",(19*sec_in_year),"seconds old.")
+print("Andrea Visanoiu is",(486180000/sec_in_year),"years old")
