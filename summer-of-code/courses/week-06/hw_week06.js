3af1c199b4807578147b58a6b32bc62d8817d2f8
Ana Sustic <sustic@gmail.com>
2018-08-30 15:19:26 +0200

    updated hw week 6

--- summer-of-code/courses/week-06/hw_week06.js
+++ summer-of-code/courses/week-06/hw_week06.js
@@ -215,8 +215,29 @@ _.each({name : "nina", age : "33", gender : "female"}, console.log);
 //_.map([[1, 2], [3, 4]], _.first);
 //=> [1, 3]
 
-var numbers = [1, 2, 3]
-var numbersMultiplied = _.map(numbers, function(value, index, items){
-    return value *3
-});
-console.log(numbersMultiplied);
+
+// Return the results of applying an iterator to each element.
+  _.map = function(collection, callBack) {
+    var mapped = [];
+    _.each(collection, function(item) {
+      mapped.push(callBack(item));
+    });
+    return mapped;
+};
+console.log(_.map([1, 2, 3], function(num){ return num * 3; }));
+console.log(_.map({one: 1, two: 2, three: 3}, function(num, key){ return num * 3; }))
+
+// reduce
+//_.reduce(list, iteratee, [memo], [context]) 
+//Aliases: inject, foldl
+//Also known as inject and foldl, reduce boils down a list of values into a single value. Memo is the initial state of the 
+//reduction, and each successive step of it should be returned by iteratee. The iteratee is passed four arguments: the memo, 
+//then the value and index (or key) of the iteration, and finally a reference to the entire list.
+
+//If no memo is passed to the initial invocation of reduce, the iteratee is not invoked on the first element of the list. 
+//The first element is instead passed as the memo in the invocation of the iteratee on the next element in the list.
+
+//var sum = _.reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0);
+//=> 6
+//https://gist.github.com/freddiecabrera/a1b13b68427c4ed641ec
+//https://underscorejs.org/docs/underscore.html
