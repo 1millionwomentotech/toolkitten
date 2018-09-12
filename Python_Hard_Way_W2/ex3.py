6b23c2ce691560e54524b6012a8ce2a738018010
Jamey Spain <jameyannehansen@gmail.com>
2018-09-12 13:04:52 -0500

    add study drill 3

---
+++ Python_Hard_Way_W2/ex3.py
@@ -0,0 +1,26 @@
+print("I will now count my chickens:")
+
+print("Hens" , 25 + 30 / 6)
+print("Roosters" , 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3+2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?" , 5 - 7)
+
+print("Oh, that's whay it's False.")
+
+print("How about some more.")
+
+print("Is it greater?" , 5 > -2)
+print("Is it greater or equal?" , 5 >= -2)
+print("Is it less or equal?" , 5 <= -2)
+
+
+print(12 % 2)
