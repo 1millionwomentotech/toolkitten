3be3b92eeb734ad93b04c2d46afea700ab3ed22d
Jamey Spain <jameyannehansen@gmail.com>
2018-09-15 16:10:29 -0500

    add drill 5

---
+++ Python_Hard_Way_W2/ex5.py
@@ -0,0 +1,24 @@
+my_name = 'Jamey A. Hansen'
+my_age = 41 # not a lie
+my_height = 65 # inches
+my_weight = 105 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+print(my_weight * my_height)
+my_weight * my_height
+print(my_weight * my_height)
+(my_weight) * (my_height)
+
