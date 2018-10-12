bcf615ed3375074b90a231c70ccf2d7fd4837efb
Kunwadee Limsawat <35984746+diakunwadee@users.noreply.github.com>
2018-10-12 13:31:23 +0700

    Add homework file

---
+++ summer-of-code/week-01/wk1-homework-submission/soc01hw-Kunwadee-Limsawat.py
@@ -0,0 +1,11 @@
+#Hours in a year. How many hours are in a year?
+Year= 52*7*24
+print (Year)
+#Minutes in a decade. How many minutes are in a decade?
+Decade= 100*52*7*24*60
+print (Decade)
+#Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
+my_age= 30*52*7*24*60*60
+print(my_age)
+#Andreea Visanoiu​: I'm 48618000 seconds old hahaha. Calculate @Andreea Visanoiu's age.
+Andreea_age = (48618000/60)/7696
